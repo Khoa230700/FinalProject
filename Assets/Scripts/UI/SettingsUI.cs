480f36088245401c70dca81b2369a7ac5e7bea12
Thái <dinhthaimap285@gmail.com>
2025-06-07 21:19:46 +0700

    Change name SettingsManagerUI.cs to SettingsUI.cs

--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsManagerUI : MonoBehaviour
+public class SettingsUI : MonoBehaviour
 {
     [Header("Panel List")]
     [SerializeField] private List<PanelOfSettings> panels = new();
