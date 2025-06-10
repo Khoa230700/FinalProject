459693d65ecdfaf04786a4007bdc11d38185e9ca
Thái <dinhthaimap285@gmail.com>
2025-06-10 16:15:02 +0700

    Add KeyBindingUI.cs

---
+++ Assets/Scripts/UI/Key Binding/KeyBindingUI.cs
@@ -0,0 +1,59 @@
+ using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindingUI : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private string actionName;
+    [SerializeField] private bool isPrimary;
+    [SerializeField] private bool saveValue;
+
+    [Header("Resource")]
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private KeyBindingPopup popup;
+
+    private string prefsKey = "KeyBinding";
+
+    private void Start()
+    {
+        LoadKey();
+        UpdateText();
+
+    }
+
+    private void SaveKey(KeyCode keyCode)
+    {
+        string key = actionName + (isPrimary ? "Primary" : "Secondary") + prefsKey;
+        PlayerPrefs.SetInt(key, (int)keyCode);
+    }
+
+    private void LoadKey()
+    {
+        string key = actionName + (isPrimary ? "Primary" : "Secondary") + prefsKey;
+        if (PlayerPrefs.HasKey(key))
+        {
+            KeyCode keyCode = (KeyCode)PlayerPrefs.GetInt(key, (int)KeyCode.None);
+            KeyBindingManager.Instance.SetKey(actionName, keyCode, isPrimary);
+        }
+    }
+
+    public void SetKey(KeyCode keyCode)
+    {
+        KeyBindingManager.Instance.SetKey(actionName, keyCode, isPrimary);
+    }
+
+    public void UpdateText()
+    {
+        var binding = KeyBindingManager.Instance.GetKeyBinding(actionName);
+        text.text = FormatKeyCode(isPrimary ? binding.primary : binding.secondary);
+    }
+
+    private string FormatKeyCode(KeyCode keyCode)
+    {
+        return (keyCode == KeyCode.None) || (keyCode == KeyCode.Escape) ? "-" : keyCode.ToString().Replace("Alpha", "");
+    }
+}
