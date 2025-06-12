97ad0d1cda94c5a93d3da4dd3227e9596f6bbb4f
Thái <dinhthaimap285@gmail.com>
2025-06-12 16:48:06 +0700

    Update WeaponSwitcher.cs

--- Assets/Scripts/Weapon/Player/WeaponSwitcher.cs
+++ Assets/Scripts/Weapon/Player/WeaponSwitcher.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 public class WeaponSwitcher : MonoBehaviour
 {
+    public WeaponUI weaponUI;
     [SerializeField] private List<GameObject> weaponList = new List<GameObject>();
 
     private int currentWeaponIndex = 0;
@@ -29,6 +31,8 @@ public class WeaponSwitcher : MonoBehaviour
         }
 
         currentWeaponIndex = index;
+
+        UpdateWeaponUI(index);
     }
 
     void ActivateWeapon(int index)
@@ -37,5 +41,32 @@ public class WeaponSwitcher : MonoBehaviour
         {
             weaponList[i].SetActive(i == index);
         }
+
+        UpdateWeaponUI(index);
+    }
+
+    private void UpdateWeaponUI(int index)
+    {
+        var activateWeapon = weaponList[index];
+        var playerShoot = activateWeapon.GetComponentInChildren<PlayerShoot>();
+
+        if (playerShoot)
+        {
+            playerShoot.weaponUI = weaponUI;
+            weaponUI.gunData = playerShoot.gunData;
+
+            weaponUI.SetWeaponSprite(playerShoot.gunData.gunSprite);
+            weaponUI.SetFireMode(playerShoot.gunData.fireMode);
+
+            weaponUI.CreateBulletUI();
+            DelayedAmmoUpdate(playerShoot);
+        }
+    }
+
+    private IEnumerator DelayedAmmoUpdate(PlayerShoot playerShoot)
+    {
+        yield return null; // chờ 1 frame
+        weaponUI.UpdateAmmoUI(playerShoot.currentAmmo, playerShoot.gunData.reserveAmmo);
     }
 }
+
