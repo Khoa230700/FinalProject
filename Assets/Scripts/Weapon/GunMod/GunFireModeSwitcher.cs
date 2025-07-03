0d53d0b257a7897c1a5014f86d2227666de48d97
SonNguyen <ncs.160820@gmail.com>
2025-07-03 17:09:01 +0700

    update gun mode and player jump

---
+++ Assets/Scripts/Weapon/GunMod/GunFireModeSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GunFireModeSwitcher : MonoBehaviour
+{
+    public GunFireMode currentFireMode = GunFireMode.FullAuto;
+
+    // Toggle giữa Burst và FullAuto bằng phím B
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SwitchFireMode();
+        }
+    }
+
+    public void SwitchFireMode()
+    {
+        if (currentFireMode == GunFireMode.Burst)
+            currentFireMode = GunFireMode.FullAuto;
+        else if (currentFireMode == GunFireMode.FullAuto)
+            currentFireMode = GunFireMode.Burst;
+
+        Debug.Log($"Switched to {currentFireMode}");
+    }
+}
