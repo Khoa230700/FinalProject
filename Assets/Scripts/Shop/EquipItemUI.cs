1620790088e023bdb8c50980babc71e0e7a064b5
Thái <dinhthaimap285@gmail.com>
2025-08-21 20:06:58 +0700

    Replace Equip Item prefab and update HUD/UI
    Removed the old Equip Item prefab and related UI elements from Canvas - HUD and InGame.unity. Added new Equip Item prefab and supporting EquipItemUI script for shop functionality, and updated UI hierarchy and visuals to match the new design.

---
+++ Assets/Scripts/Shop/EquipItemUI.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipItemUI : MonoBehaviour
+{
+    public TMP_Text costText;
+    public Transform upgradeBarParent;
+    public Image avatar;
+    public TMP_Text ammo;
+}
