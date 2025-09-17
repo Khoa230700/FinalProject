b30d6ab8419e33fa0e18809ca3f44fd955b4d612
Khoa230700 <yujidream2307@gmail.com>
2025-09-17 17:27:46 +0700

    trigger cutsence

---
+++ Assets/Scripts/Bot/CutSence.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSence : MonoBehaviour
+{
+    public PlayableDirector cutscene;  // kéo PlayableDirector vào
+    private bool hasPlayed = false;    // tránh play nhiều lần
+    public GameObject botmelee;
+    public GameObject botcutscene;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasPlayed) return;
+
+        if (other.CompareTag("BotMelee")) // nếu nhân vật có tag "Player"
+        {
+            botmelee.SetActive(false);
+            botcutscene.SetActive(true);
+            cutscene.Play();
+            hasPlayed = true;
+            Debug.Log("Cutscene Started!");
+        }
+    }
+}
