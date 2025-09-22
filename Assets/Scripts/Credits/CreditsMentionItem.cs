9195a76c753c15131a842b2cb4bc4cc8c066b4ca
Thái <dinhthaimap285@gmail.com>
2025-09-22 23:07:09 +0700

    10

--- Assets/Scripts/Credits/CreditsMentionItem.cs
+++ Assets/Scripts/Credits/CreditsMentionItem.cs
@@ -2,45 +2,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-namespace Michsky.UI.Heat
+public class CreditsMentionItem : MonoBehaviour
 {
-    public class CreditsMentionItem : MonoBehaviour
+    [Header("Resources")]
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    public VerticalLayoutGroup listLayout;
+
+    // Helpers
+    [HideInInspector] public CreditsPreset preset;
+
+    public void UpdateLayout(int paddingValue, int spacingValue)
     {
-        [Header("Resources")]
-        [SerializeField] private Image iconImage;
-        [SerializeField] private TextMeshProUGUI descriptionText;
-        public VerticalLayoutGroup listLayout;
+        listLayout.padding.top = paddingValue / 2;
+        listLayout.padding.bottom = paddingValue / 2;
+        listLayout.spacing = spacingValue;
+    }
 
-        // Helpers
-        [HideInInspector] public CreditsPreset preset;
-
-        public void UpdateLayout(int paddingValue, int spacingValue)
+    public void SetIcon(Sprite icon)
+    {
+        if (icon == null)
         {
-            listLayout.padding.top = paddingValue / 2;
-            listLayout.padding.bottom = paddingValue / 2;
-            listLayout.spacing = spacingValue;
+            iconImage.gameObject.SetActive(false);
+            return;
         }
 
-        public void SetIcon(Sprite icon)
-        {
-            if (icon == null)
-            {
-                iconImage.gameObject.SetActive(false);
-                return;
-            }
+        iconImage.sprite = icon;
+    }
 
-            iconImage.sprite = icon;
+    public void SetDescription(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            descriptionText.gameObject.SetActive(false);
+            return;
         }
 
-        public void SetDescription(string text)
-        {
-            if (string.IsNullOrEmpty(text)) 
-            {
-                descriptionText.gameObject.SetActive(false);
-                return;
-            }
-
-            descriptionText.text = text;
-        }
+        descriptionText.text = text;
     }
 }
