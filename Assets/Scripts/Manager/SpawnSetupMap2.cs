12bf0adf24923e2708e264bd4c16f1e446fb8fb9
Thanh8369 <mineski.3010@gmail.com>
2025-09-22 22:13:06 +0700

    Update Map2

---
+++ Assets/Scripts/Manager/SpawnSetupMap2.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSetupMap2 : MonoBehaviour
+{
+    public Map2SpawnController map2SpawnController;
+
+    private void Start()
+    {
+        List<WaveData> waves = new List<WaveData>();
+
+        waves.Add(new WaveData
+        {
+            enemies = new List<EnemySpawnInfo>
+            {
+                new EnemySpawnInfo { enemyType = EnemyType.Nor, count = 15 },
+                new EnemySpawnInfo { enemyType = EnemyType.Fat, count = 5 }
+            }
+        });
+
+        waves.Add(new WaveData
+        {
+            enemies = new List<EnemySpawnInfo>
+            {
+                new EnemySpawnInfo { enemyType = EnemyType.Nor, count = 15 },
+                new EnemySpawnInfo { enemyType = EnemyType.Fat, count = 5 }
+            }
+        });
+        map2SpawnController.SetWaves(waves);
+        map2SpawnController.StartGame();
+    }
+}
