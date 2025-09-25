cc60520c5d11a42d60ca53ce67dda1c6d3ca76cc
SonNguyen <ncs.160820@gmail.com>
2025-09-25 23:41:15 +0700

    update cutscene map 3

--- Assets/Scripts/Selector/SelectorSpawner.cs
+++ Assets/Scripts/Selector/SelectorSpawner.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 public class SelectorSpawner : MonoBehaviour
@@ -21,6 +21,9 @@ public class SelectorSpawner : MonoBehaviour
 
     private int selectedIndex;
 
+    // giữ reference crosshair runtime
+    private CrosshairBloomController _crosshair;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -36,8 +39,17 @@ public class SelectorSpawner : MonoBehaviour
         // Spawn player
         Player = Instantiate(playerPfs[selectedIndex].playerPrefab, transform.position, transform.rotation);
 
+        // TÌM crosshair ở trong prefab player (kể cả inactive)
+        _crosshair = Player.GetComponentInChildren<CrosshairBloomController>(true);
+
         playerClassNameUI.UpdateUI(playerPfs[selectedIndex]);
+
+        // (tuỳ chọn) đảm bảo crosshair bật khi mới vào scene
+        _crosshair?.Show();
     }
+    // ====== API cho Timeline (Signal Receiver bind vào 2 hàm này) ======
+    public void HideCrosshair() => _crosshair?.Hide();
+    public void ShowCrosshair() => _crosshair?.Show();
 }
 
 [Serializable]
